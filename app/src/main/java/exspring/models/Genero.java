2d349032c892edf6c0f5760f0b040aa5ac13bb74
Marco Carvalho <marcocarvalho.web@gmail.com>
2022-09-19 13:50:00 -0300

    init

---
+++ app/src/main/java/exspring/models/Genero.java
@@ -0,0 +1,32 @@
+package exspring.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
