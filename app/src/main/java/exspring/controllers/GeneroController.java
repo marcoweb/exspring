cb3c857eb0ded7ca2aa18409b48469195321511b
Marco Carvalho <marcocarvalho.web@gmail.com>
2022-09-19 14:55:56 -0300

    generos

--- app/src/main/java/exspring/controllers/GeneroController.java
+++ app/src/main/java/exspring/controllers/GeneroController.java
@@ -1,13 +1,21 @@
 package exspring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import exspring.repositories.GeneroRepository;
 
 @Controller
 @RequestMapping("/generos")
 public class GeneroController {
+    @Autowired
+    private GeneroRepository generosRepo;
+
     @RequestMapping("list")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("generos", this.generosRepo.findAll());
         return "list";
     }
 }
